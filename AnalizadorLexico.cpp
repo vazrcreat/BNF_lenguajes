656a9e1ee712dcf087ff3d9ab469b009b480f5a7
YeseniaEsp29 <80561970+YeseniaEsp29@users.noreply.github.com>
2021-04-13 19:18:21 -0600

    Update AnalizadorLexico.cpp

--- AnalizadorLexico.cpp
+++ AnalizadorLexico.cpp
@@ -95,17 +95,231 @@ void Analiza::devuelveToken(char token){
 }
 
 int main(int argc, char** argv){
-	int traza;
-	char token;
-	
-	Analiza obj("commandos.txt",1);
-	
-	if(obj.existeTraza())
-	cout<<"INICIO DE ANALISIS"<<endl;
-	
-	while((token=obj.siguienteToken())!='}')
-	cout<<token<<endl;
-	
-	system("pause");
-	return 0;
+	system("COLOR 0A");
+   char entrada[MAX];
+   char *p = entrada;
+   printf("Ingrese la primera linea del lenguaje: ");
+   fflush(stdin);
+   gets(entrada);
+	fflush(stdin);
+   //clrscr(entrada);
+   system("COLOR 0B");
+
+   char palabra[MAX]; //Esta variable es muy importante, pues guardara caracter por caracter formando asi una palabra y termina de guardar cuando detecta un espacio.
+   int indexPalabra=0;
+   int indexSimbolos = 0;
+
+   //Ciclos para Iniciar diferentes arreglos.
+   for(int i=0;i<MAX;i++){tokenReservadas[i] = "";} // Fin del ciclo
+
+   for(int i=0;i<MAX;i++){tokensNoValidos[i] = "";}
+
+   for(int i=0;i<MAX;i++){
+		palabra[i] = '\0';
+   }
+
+   //Este bucle es el que recorre cada caracter de la entrada(programa fuente) que ingreso el usuario, el procedimiento dentro del bucle es para formar los tokens necesarios.
+   while(*p!='\0')
+   {
+		if((*p != ' ') && (*p !=','))  	//Primero revisa que sea distinto de un espacio ' ' y una coma ','
+      {
+      	if((*p== ';') || (*p=='"') || (*p=='(') || (*p==')') || (*p=='+') || (*p=='-') || (*p=='*') || (*p=='/') || (*p=='#') || (*p=='<') || (*p=='>') || (*p=='='))
+	      {
+      	   tokenSimbolos[indexSimbolos] = *p;
+         	indexSimbolos++;
+	      }
+         else
+         {
+				palabra[indexPalabra] = *p;
+            auxWord[indexPalabra] = *p;	//La razon de usar este arreglo es para mandar como parametro al metodo verificarNumero()... debido a un problema de programacion.
+            indexPalabra++;
+         }
+      }
+      else if((*p ==' ') || (*p ==','))
+      {
+      	// Guarda la palabra en los respectivos tokens segun las funciones hayan examinado la palabra.
+         if(verificarReservada(palabra) == true){
+				tokenReservadas[auxTR] = palabra;
+          	auxTR++;
+         }else if(verificarIdentificador(palabra) == true){
+         	tokenIdentificadores[auxTI] = palabra;
+		      auxTI++;
+         }else if(verificarNumero(auxWord)==true){
+         	tokenNumeros[auxTN] = auxWord;
+		      auxTN++;
+         }
+         for(int i=0;i<MAX;i++){palabra[i]='\0';}	//Para vaciar el arreglo de la palabra
+         for(int i=0;i<MAX;i++){auxWord[i]='\0';}	//Para vaciar el arreglo del arreglo auxiliar
+         indexPalabra=0;
+      }
+      p++;
+   }
+
+   //Esto es para revisar la ultima palabra formada que no se pudo examinar en el bucle while anterior
+	if(verificarReservada(palabra) == true){
+   	tokenReservadas[auxTR] = palabra;
+    	auxTR++;
+   }else if(verificarIdentificador(palabra) == true){
+		tokenIdentificadores[auxTI] = palabra;
+		auxTI++;
+   }else if(verificarNumero(auxWord)==true){
+    	tokenNumeros[auxTN] = auxWord;
+      auxTN++;
+   }
+
+   //FINALMENTE SE LLAMAN A IMPRIMIR LOS RESULTADOS OBTENIDOS.
+   printf("\t\tEXPRESION: %s\n",entrada);
+   imprimirTokenReservadas();
+   imprimirTokenSimbolos();
+   imprimirTokenIdentificadores();
+   imprimirTokenNumeros();
+   imprimirIdentificadoresNoValidos();
+	getch();
+}
+
+
+//Inicio del desarrollo de los metodos para imprimir los resultados.
+void imprimirTokenSimbolos(){
+	puts("\n\n----------------\n|TOKENS SIMBOLOS|\n----------------");
+	for(int i=0;i<strlen(tokenSimbolos);i++){printf("%c\n",tokenSimbolos);}
+}
+
+void imprimirTokenReservadas(){
+	puts("\n-----------------------\n|TOKENS Reservadas|\n-----------------------");
+    for(int i=0;i<auxTR;i++){printf("%d %s\n",i+1,tokenReservadas);}
+}
+
+void imprimirTokenIdentificadores(){
+	puts("\n-----------------------\n|TOKENS IDENTIFICADORES|\n-----------------------");
+	for(int i=0;i<auxTI;i++){printf("%d %s\n",i+1,tokenIdentificadores);}
+}
+
+void imprimirTokenNumeros(){
+	puts("\n-----------------------\n|TOKENS NUMEROS|\n-----------------------");
+    for(int i=0;i<auxTN;i++){printf("%d %s\n",i+1,tokenNumeros);}
+}
+
+void imprimirIdentificadoresNoValidos(){
+	puts(" ");
+	printf("********************************* MENSAJES *********************************\n");
+   if(auxTNV == 0){
+   	printf("\t\t 0 Errores, programa fuente exitoso...");
+   }else{
+   	for(int i=0;i<auxTNV;i++)
+	   {
+   		printf("\t\tERROR: No es Valido el Token: %s \n",tokensNoValidos);
+   	}
+   }
+}
+
+//Fin del desarrollo de metodos para imprimir
+
+// Metodo para verificar si el token formado corresponde a una palabra reservada
+bool verificarReservada(char palabra[]){
+	int comp;
+   bool esReservada=false;
+   string str(palabra);
+	for(int i=0; i<MAX_RES; i++)
+   {
+   	comp = strcmp(PalabrasReservadas[i],palabra);
+      if(comp == 0)
+      {
+         esReservada = true;
+         break;
+      }
+   }
+   return esReservada;
+}
+
+// Metodo para verificar si el token formado cumple para ser un identificador
+bool verificarIdentificador(char palabra[]){
+	string auxPalabra = palabra;
+   esIdentificador = false;
+   short estado = 0;
+   char *p = palabra;
+   while(*p != '\0')
+   {
+   	switch(estado)
+      {
+      	case 0:
+         	if((isalpha(*p)) || (*p=='_')){
+            	estado = 1;
+               esIdentificador = true;
+            }
+            else{
+            	estado = 2;
+               esIdentificador = false;
+            }
+            p++;
+         break;
+         case 1:
+         	if((isalpha(*p)) || (isdigit(*p)) || (*p=='_')){
+            	estado = 1;
+               esIdentificador = true;
+            }
+            else{
+            	estado = 2;
+               esIdentificador = false;
+            }
+            p++;
+         break;
+         case 2:
+				//No es un identificador
+            esIdentificador = false;
+            *p = '\0';
+         break;
+      }
+   }
+   return esIdentificador;
+}
+
+//Metodo para revisar si el token formado cumple para ser un numero entero o decimal. Aparte, aqui se guardan los tokens No Validos para mostrarlos en el metodo imprimirIdentificadoresNoValidos()
+bool verificarNumero(char palabra[]){
+	string auxPalabra = palabra;
+   esNumero = false;
+	short estado = 0,cont=0;
+   char *p = palabra;
+	while(*p!='\0')
+   {
+   	switch(estado)
+      {
+      	case 0:
+            if(isdigit(*p)){
+					estado = 0;
+               esNumero = true;
+               cont++;
+            }
+            else if( ((*p == '.') && (cont==0)) || (isalpha(*p))){
+					estado = 2;
+               esNumero=false;
+            }else if(*p=='.'){
+					estado = 1;
+            	esNumero = false;
+            }
+         	p++;
+         break;
+         case 1:
+            if(isdigit(*p)){
+            	estado = 1;
+               esNumero = true;
+            }else
+            {
+            	estado = 2;
+               esNumero = false;
+            }
+         	p++;
+         break;
+         case 2:
+				esNumero = false;
+         	*p = '\0';
+         break;
+      }
+   }
+   if(esNumero == false){
+      if(auxPalabra != ""){
+   	   tokensNoValidos[auxTNV] = auxPalabra;
+	      auxTNV++;
+      }
+   }
+ 	return esNumero;
 }
